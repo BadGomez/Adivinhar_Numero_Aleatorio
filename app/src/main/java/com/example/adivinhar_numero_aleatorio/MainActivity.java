ffaa448ad6f402c874a08f9f1faab0803ad672a2
Valter Simões <valterrsimoes@gmail.com>
2020-04-17 19:06:23 +0100

    Layout concluido; Keys realizadas para cada caixa de texto; Possibilidade da aplicação para telemoveis com idioma em Portugues e Inglês

--- app/src/main/java/com/example/adivinhar_numero_aleatorio/MainActivity.java
+++ app/src/main/java/com/example/adivinhar_numero_aleatorio/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.adivinhar_numero_aleatorio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private Random random = new Random();
+    private int numeroAdivinhar = random.nextInt(10) + 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void TentarNumero (View view){
+
     }
 }
